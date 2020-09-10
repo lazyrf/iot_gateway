36c09c8189fa1274d304c889f3c35474d117384e
Aron <lazyrf.tw@gmail.com>
2020-09-10 13:11:46 +0800

    Implement drv_core to init systick

---
+++ bsp/driver/drv_core.c
@@ -0,0 +1,13 @@
+#include "drv_core.h"
+#include "stm32f4xx.h"
+
+void drv_core_systick_init(void)
+{
+    /* SystemFrequency / 1000 ==> 1ms interrupt */
+    /* SystemFrequency / 100000 == > 10us interrupt */
+    /* SystemFrequency / 1000000 ==> 1us interrupt */
+    if (SysTick_Config(SystemCoreClock / 1000)) {
+        while (1);
+    }
+}
+
---
+++ bsp/driver/drv_core.h
@@ -0,0 +1,6 @@
+#ifndef __DRV_CORE_H
+#define __DRV_CORE_H
+
+void drv_core_systick_init(void);
+
+#endif /* __DRV_CORE_H */
