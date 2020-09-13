50ffaba45ad26648f94c4b75e78736fcd75db087
Aron <lazyrf.tw@gmail.com>
2020-09-13 16:00:29 +0800

    Implement drv_misc

---
+++ bsp/driver/drv_misc.c
@@ -0,0 +1,20 @@
+#include "drv_misc.h"
+#include "board.h"
+#include "hal_iwdg.h"
+#include "hal_core.h"
+
+#if BOARD_WATCHDOG_ENABLE
+void drv_misc_iwdg_feed(void)
+{
+    hal_iwdg_feed();
+}
+#endif /* BOARD_WATCHDOG_ENABLE */
+
+void drv_misc_init(void)
+{
+    hal_core_systick_init(1000);
+
+#if BOARD_WATCHDOG_ENABLE
+    hal_iwdg_init();
+#endif /* BOARD_WATCHDOG_ENABLE */
+}
---
+++ bsp/driver/drv_misc.h
@@ -0,0 +1,7 @@
+#ifndef __DRV_MISC_H
+#define __DRV_MISC_H
+
+void drv_misc_iwdg_feed(void);
+void drv_misc_init(void);
+
+#endif /* __DRV_MISC_H */
